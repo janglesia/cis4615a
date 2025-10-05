a0e89b4afc8fb7c244cc5a4ebd9ca0d3b4bd3dbc
Angie J <87537204+janglesia@users.noreply.github.com>
2025-10-05 11:34:19 -0400

    Update R04_STR03_J.java
    COMPLIANT SOLUTION

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -5,15 +5,15 @@
 *   
 *   Rule 04. Characters and Strings (STR)
 *   STR03-J. Do not encode noncharacter data as a string
-*   NON-COMPLIANT CODE
+*   COMPLIANT SOLUTION
 */
 
 public class R04_STR03_J {
     public static void main(String[] args) {
         BigInteger x = new BigInteger("530500452766");
-        byte[] byteArray = x.toByteArray();
-        Strings = new String(byteArray);
-        byteArray = s.getBytes();
-        x = new BigInteger(byteArray);
+        String s = x.toString(); // Valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray);
+        x = new BigInteger(ns);
     }
 }
