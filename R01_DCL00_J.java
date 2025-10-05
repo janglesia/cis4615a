206376614a4a7720f7a855f02b92a90ebf24c513
Angie J <87537204+janglesia@users.noreply.github.com>
2025-10-05 11:33:08 -0400

    Update R01_DCL00_J.java

--- R01_DCL00_J.java
+++ R01_DCL00_J.java
@@ -5,13 +5,13 @@
 *   
 *   Rule 01. Declarations and Initialization (DCL)
 *   DCL00-J. Prevent class initialization cycles
-*   NON-COMPLIANT CODE
+*   COMPLIANT SOLUTION
 */
 
 public class R01_DCL00_J {
     private final int balance;
-    private static final Cycle c = new Cycle();
     private static final int deposit = (int) (Math.random() * 100); // Random deposit
+    private static final Cycle c = new Cycle();  // Inserted after initialization of required fields
     public Cycle() {
         balance = deposit - 10; // Subtract processing fee
     }
