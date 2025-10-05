725c18f809f029c412d668ced4a16f11847fb21b
Angie J <87537204+janglesia@users.noreply.github.com>
2025-10-05 11:35:08 -0400

    Update R06_MET01_J.java
    COMPLIANT SOLUTION

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -5,17 +5,20 @@
 *   
 *   Rule 06. Methods (MET)
 *   MET01-J. Never use assertions to validate method arguments
-*   NON-COMPLIANT CODE
+*   COMPLIANT SOLUTION
 */
 
 public class R06_MET01_J {
     public static int getAbsAdd(int x, int y) {
-        assert x != Integer.MIN_VALUE;
-        assert y != Integer.MIN_VALUE;
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE){
+            throw new IllegalArgumentException();
+        }
         int absX = Math.abs(x);
         int absY = Math.abs(y);
-        assert (absX <= Integer.MAX_VALUE - absY);
+        if (absX > Integer.MAX_VALUE - absY){
+            throw new IllegalArgumentException();
+        }
         return absX + absY;
     }
-    // Usage : getAbsAdd ( I n t e g e r .MIN VALUE, 1 ) ;
+    
 }
